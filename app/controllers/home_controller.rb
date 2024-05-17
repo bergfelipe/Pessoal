e07e6e89a15e34a056186a714e2d3d34e03f8b93
bergfelipe <felipe4bfonseca@gmail.com>
2024-05-17 17:58:13 -0400

    add ransack

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @q = Consultum.ransack(params[:q])
+    @pagy, @consultas = pagy(@q.result(distinct: true))
+    @medicos = Medico.all
   end
 end
--- app/models/consultum.rb
+++ app/models/consultum.rb
@@ -3,4 +3,13 @@ class Consultum < ApplicationRecord
   belongs_to :paciente
 
   accepts_nested_attributes_for :paciente, reject_if: :all_blank, allow_destroy: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "data_consulta", "ficha", "id", "id_value", "medico_id", "paciente_id", "tipo", "updated_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["medico", "paciente"]
+  end
+
 end
