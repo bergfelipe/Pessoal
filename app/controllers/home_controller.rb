f5ccee0d702da86a4c74f7dcbc990d596a429f9e
bergfelipe <felipe4bfonseca@gmail.com>
2024-06-07 19:20:55 -0400

    junta front com o back

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
   def index
     @q = Consultum.ransack(params[:q])
-    @pagy, @consultas = pagy(@q.result(distinct: true))
+    @pagy, @consultas = pagy(@q.result(distinct: true).order(created_at: :desc))    
     @medicos = Medico.all
+    @pacientes = Paciente.all
   end
 end
