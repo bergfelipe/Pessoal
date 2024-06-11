b4097b89a4132a7930385dd196f266bc57beae55
bergfelipe <felipe4bfonseca@gmail.com>
2024-06-11 16:05:41 -0400

    add regra de tipo de perfil

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,7 @@ class HomeController < ApplicationController
     @pagy, @consultas = pagy(@q.result(distinct: true).order(created_at: :desc))    
     @medicos = Medico.all
     @pacientes = Paciente.all
+    @medico_id = Medico.where(cpf: current_admin.cpf)
+    @consulta_comp = Consultum.where(medico_id: @medico_id.ids)
   end
 end
