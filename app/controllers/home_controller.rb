7e230181aef5651fba94a52d67376bf0f106280f
bergfelipe <felipe4bfonseca@gmail.com>
2024-06-15 16:56:05 -0400

    faz helper de minutos faltanetes funcionar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,17 @@
 class HomeController < ApplicationController
   def index
     @q = Consultum.ransack(params[:q])
-    @pagy, @consultas = pagy(@q.result(distinct: true).order(created_at: :desc))
+    @pagy, @consultas = pagy(@q.result(distinct: true).order(data_consulta: :desc))
     @medicos = Medico.all
     @pacientes = Paciente.all
-    @medico_id = Medico.where(cpf: current_admin.cpf)
-    @consulta_comp = Consultum.where(medico_id: @medico_id.ids).order(created_at: :desc)
+    # Encontre o médico pelo CPF do administrador atual
+    @medico_id = Medico.where(cpf: current_admin.cpf).ids
+
+    # Filtre as consultas que pertencem ao médico encontrado e que a data_consulta é o dia presente
+    @consulta_comp = Consultum.where(medico_id: @medico_id)
+                              .where('DATE(data_consulta) = ?', Date.current)
+                              .order(data_consulta: :desc)
   end
 end
+
+
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,11 +4,50 @@ module ApplicationHelper
     end
 
     def tempo_restante(data_consulta)
-        diferenca = ((data_consulta - Time.now) / 60).to_i
-        if diferenca > 0
-          "daqui a #{diferenca} minutos"
-        else
-          "consulta já ocorreu"
-        end
+      # Certifique-se de que data_consulta seja um objeto Time
+      data_consulta = data_consulta.to_time if data_consulta.respond_to?(:to_time)
+    
+      # Use Time.now para obter o horário atual
+      now = Time.now
+    
+      # Verifique se a data da consulta é no mesmo dia
+      if data_consulta.to_date != now.to_date
+        return "consulta não é hoje"
       end
+    
+      # Extraia a hora e os minutos de data_consulta e now
+      data_consulta_time = Time.new(now.year, now.month, now.day, data_consulta.hour, data_consulta.min, 0)
+      now_time = Time.new(now.year, now.month, now.day, now.hour, now.min, 0)
+    
+      # Calcule a diferença em segundos
+      diferenca_em_segundos = data_consulta_time - now_time
+    
+      # Verifique se a consulta já ocorreu
+      if diferenca_em_segundos <= 0
+        return "consulta já ocorreu"
+      end
+    
+      # Converta a diferença para minutos
+      diferenca_em_minutos = (diferenca_em_segundos / 60).to_i
+    
+      # Mostre a diferença em horas se for maior que 60 minutos
+      if diferenca_em_minutos >= 60
+        horas_restantes = diferenca_em_minutos / 60
+        minutos_restantes = diferenca_em_minutos % 60
+        if minutos_restantes > 0
+          "faltam #{horas_restantes} horas e #{minutos_restantes} minutos para a consulta"
+        else
+          "faltam #{horas_restantes} horas para  para a consulta"
+        end
+      else
+        "faltam #{diferenca_em_minutos} minutos para  para a consulta"
+      end
+    end
+    
+    
+    
+    
+    
+    
+    
 end
